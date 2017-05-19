13eb77ead31b13422b51d035ca8e03b481dc1eb9
innyso <mini.inny@gmail.com>
2017-05-19 11:18:13 +1000

    Add ability to set waiting time
    Allow user to define waiting time so that user will not go into an
    infinite loop when one of the service keep crashing or not running.
    
    To set waiting time, define an environment variable WAIT_FOR=<time in
    second>. Default to no waiting time defined.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
-
 : ${SLEEP_LENGTH:=2}
+
+SLEPT=0
+
+keep_waiting(){
+  if [ -n "$WAIT_FOR" ]; then
+    if [ "$WAIT_FOR" -gt "$SLEPT" ]; then
+      echo "waiting for ${WAIT_FOR}s current ${SLEPT}s"
+      return 0
+    else
+      echo "Exceed waiting limit $SLEPT"
+      return 1
+    fi
+  else
+    return 0
+  fi
+}
 
 wait_for() {
   echo Waiting for $1 to listen on $2...
-  while ! nc -z $1 $2; do echo sleeping; sleep $SLEEP_LENGTH; done
+  while keep_waiting && ! nc -z $1 $2; do echo sleeping; sleep $SLEEP_LENGTH; SLEPT=$((SLEEP_LENGTH+SLEPT)); done
 }
 
 for var in "$@"
