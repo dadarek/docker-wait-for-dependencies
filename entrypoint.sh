35d13da81b1a3ba5c47a14c9d56dfe032009498a
Miki Grgic <m.grgic@pamet.de>
2018-04-09 13:09:08 +0200

    implemented timeout to cancel waiting for unreachable services

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
 : ${SLEEP_LENGTH:=2}
+: ${TIMEOUT_LENGTH:=300}
 
 wait_for() {
+  START=$(date +%s)
   echo Waiting for $1 to listen on $2...
-  while ! nc -z $1 $2; do echo sleeping; sleep $SLEEP_LENGTH; done
+  while ! nc -z $1 $2;
+    do
+    if [ $(($(date +%s) - $START)) -gt $TIMEOUT_LENGTH ]; then
+        echo timeout
+        break
+    fi
+    echo sleeping;
+    sleep $SLEEP_LENGTH;
+  done
 }
 
 for var in "$@"
